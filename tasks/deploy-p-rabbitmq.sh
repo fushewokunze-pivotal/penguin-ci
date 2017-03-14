622da59ce641d29cea606a1a694b6bfd7ab66c10
Fadzi Ushewokunze <fushewokunze@pivotal.io>
2017-03-14 00:19:41 -0400

    Update deploy-p-rabbitmq.sh

--- tasks/deploy-p-rabbitmq.sh
+++ tasks/deploy-p-rabbitmq.sh
@@ -10,13 +10,9 @@ cat <<RABBITMQVARS > p-rabbitmq-vars-template.yml
 ########################
 # Cloud Foundry config #
 ########################
-cf_api_url: https://api.$(jq -r .sys_domain terraform-state/metadata)
-cf_uaa_admin_client_secret: ((/aws-director-credhub/cf/apps_manager_client_password))
-cf_admin_username: push_apps_manager
-cf_admin_password: ((/aws-director-credhub/cf/push_apps_manager_password))
-cf_app_domains: [$(jq -r .apps_domain terraform-state/metadata)]
-cf_sys_domain: $(jq -r .sys_domain terraform-state/metadata)
-cf_skip_ssl_validation: true
+apps_domain: [$(jq -r .apps_domain terraform-state/metadata)]
+system_domain: $(jq -r .sys_domain terraform-state/metadata)
+skip_ssl_validation: true
 nats:
   password: ((/aws-director-credhub/cf/nats_password))
   machines: ((nats_ips))
