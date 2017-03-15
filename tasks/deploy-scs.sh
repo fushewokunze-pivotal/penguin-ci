9bf88617bc3d7175d7a525a99b13ed470b19e382
Fadzi Ushewokunze <fadzi@butterflycluster.com>
2017-03-15 09:12:37 -0400

    remove var scs store

--- tasks/deploy-scs.sh
+++ tasks/deploy-scs.sh
@@ -4,7 +4,7 @@ set -e
 
 eval "$(pcf-bosh-ci/scripts/director-environment bosh-vars-store/*-bosh-vars-store.yml terraform-state/metadata)"
 
-cat <<SCSVARS > scs-vars-template.yml
+cat <<SCSVARS > scs-vars.yml
 ---
 
 ########################
@@ -14,8 +14,6 @@ apps_domain: [$(jq -r .apps_domain terraform-state/metadata)]
 system_domain: $(jq -r .sys_domain terraform-state/metadata)
 skip_ssl_validation: true
 SCSVARS
-
-cp scs-vars-store/*-scs-vars-store.yml new-scs-vars-store/scs-vars-store.yml
 
 bosh -n deploy penguin-ci/manifests/scs/p-scs.yml \
   --deployment p-scs \
