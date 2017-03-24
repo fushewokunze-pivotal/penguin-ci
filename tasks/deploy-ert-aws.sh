672fbfe06315020a84f23caf7a2d810270b707cd
Fadzi Ushewokunze <fushewokunze@pivotal.io>
2017-03-23 22:03:04 -0400

    Update deploy-ert-aws.sh

--- tasks/deploy-ert-aws.sh
+++ tasks/deploy-ert-aws.sh
@@ -10,6 +10,7 @@ bosh -n deploy cf-deployment/cf-deployment.yml \
   --deployment cf \
   --ops-file cf-deployment/opsfiles/change-logging-port-for-aws-elb.yml \
   --ops-file cf-deployment/opsfiles/disable-router-tls-termination.yml \
+  --ops-file penguin-ci/ops-files/ha_proxy.yml \
   --ops-file p-ert/pivotal-defaults.yml \
   --ops-file p-ert/mysql-proxy.yml \
   --ops-file p-ert/mysql-monitoring.yml \
